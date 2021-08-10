c57d44abddd75e561c0bbced9491ac886eeeaba6
gcontarini <>
2021-08-10 21:37:22 +0100

    Create make

--- server/server.c
+++ server/server.c
@@ -1,62 +1,48 @@
+#include "srvr.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/times.h>
-#include <sys/select.h>
-#include <unistd.h>
+int socket_listening;
+fd_set select_set;
 
-#define SERVER_BUSY "Sorry. The server is busy. bye\n"
+int  *list_of_sockets;
 
-#define MAX_USERS 20
-#define MSG_LEN 4096
+int  max_users = MAX_USERS;
 
-int socket_listening ;
-fd_set select_set ;
+int  list_len = 0;
 
-int  *list_of_sockets ;
-
-int  max_users = MAX_USERS ;
-
-int  list_len = 0 ;
-
-char msg[MSG_LEN+1] ;
+char msg[MSG_LEN+1];
 
 void show_help(char *name) {
-printf("Chatserv 1.0\n") ;
-printf("by Daemonio (undefinido gmail com)\n\n") ;
-printf("[uso] %s <port> [<max_users>]\n", name) ;
+printf("Chatserv 1.0\n");
+printf("by Daemonio (undefinido gmail com)\n\n");
+printf("[uso] %s <port> [<max_users>]\n", name);
 }
 
 char insert_socket_into_list(int socket) {
     int i ;
 
-    if ( list_len == max_users ) {
-        return 1 ;
+    if (list_len == max_users) {
+        return (1);
     }
 
-    for ( i = 0; i < max_users; i++ ) {
-        if ( list_of_sockets[i] == -1 ) {
-            list_of_sockets[i] = socket ;
-            list_len++ ;
-            break ;
+    for (i = 0; i < max_users; i++) {
+        if (list_of_sockets[i] == -1) {
+            list_of_sockets[i] = socket;
+            list_len++;
+            break;
         }
     }
-    return 0 ;
+    return (0);
 }
 
 void remove_socket_from_list(int _sock) {
-    int i ;
+    int i;
 
-    for ( i = 0; i < max_users; i++ ) {
-        if ( list_of_sockets[i] == _sock ) {
-            close(list_of_sockets[i]) ;
-            list_of_sockets[i] = -1 ;
-            list_len-- ;
-            break ;
+    for (i = 0; i < max_users; i++) {
+        if (list_of_sockets[i] == _sock) {
+            close(list_of_sockets[i]);
+            list_of_sockets[i] = -1;
+            list_len--;
+            break;
         }
     }
 }
@@ -83,82 +69,77 @@ void send_message_to_all(int _sock) {
 }
 
 int main() {
+    int t;
     int port = 6000;
-    int t    ;
 
-    struct sockaddr_in server ;
-    struct timeval select_time ;
+    struct sockaddr_in server;
+    struct timeval select_time;
 
-    socket_listening = socket(AF_INET, SOCK_STREAM, 0) ;
-
-    if ( socket_listening < 0 ) {
-        perror("socket") ;
-        return -1 ;
+    socket_listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_listening < 0) {
+        perror("socket");
+        return (-1);
     }
 
-    server.sin_family = AF_INET ;
-    server.sin_port = htons(port) ;
-    server.sin_addr.s_addr = INADDR_ANY ;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = INADDR_ANY;
 
-    t = sizeof(struct sockaddr_in) ;
-    if ( bind( socket_listening, (struct sockaddr *) &server, t ) < 0 ) {
-        perror("bind") ;
-        return -1 ;
+    t = sizeof(struct sockaddr_in);
+    if (bind(socket_listening, (struct sockaddr *) &server, t) < 0) {
+        perror("bind");
+        return (-1);
     }
-
-    if ( listen(socket_listening, 5) < 0 ) {
-        perror("listen") ;
-        return -1 ;
+    if (listen(socket_listening, 5) < 0) {
+        perror("listen");
+        return (-1);
     }
-
+    
     list_of_sockets = (int *) malloc( max_users * sizeof(int) ) ;
-    if ( list_of_sockets == NULL ) {
-        perror("malloc") ;
-        return -1 ;
+    if (list_of_sockets == NULL) {
+        perror("malloc");
+        return (-1);
     }
 
-    for ( t = 0; t < max_users; t++ )
-        list_of_sockets[t] = -1 ;
+    for (t = 0; t < max_users; t++)
+        list_of_sockets[t] = -1;
+    while (1) {
+        FD_ZERO(&select_set);
+        FD_SET(socket_listening, &select_set);
+        for (t = 0; list_len > 0 && t < max_users; t++)
+            if (list_of_sockets[t] != -1)
+                FD_SET(list_of_sockets[t], &select_set);
+        printf("[+] Listening on %d [%d/%d] ...\n", port, list_len, max_users);
+        select_time.tv_sec = 1;
+        select_time.tv_usec = 0;
 
-    while ( 1 ) {
-        FD_ZERO(&select_set) ;
-        FD_SET(socket_listening, &select_set) ;
-        for ( t = 0; list_len > 0 && t < max_users; t++ )
-            if ( list_of_sockets[t] != -1 )
-                FD_SET(list_of_sockets[t], &select_set) ;
-
-        printf("[+] Listening on %d [%d/%d] ...\n", port, list_len, max_users) ;
-
-        select_time.tv_sec = 1 ;
-        select_time.tv_usec = 0 ;
-
-        if ( (t=select(FD_SETSIZE, &select_set, NULL, NULL, &select_time)) < 0 ) {
-            perror("select") ;
-            return -1 ;
+        if ((t=select(FD_SETSIZE, &select_set, NULL, NULL, &select_time)) < 0) {
+            perror("select");
+            return (-1);
         }
 
-        if ( t > 0 ) {
-            if ( FD_ISSET(socket_listening, &select_set) )
+        if (t > 0) {
+            if (FD_ISSET(socket_listening, &select_set))
             {
-                int n ;
+                int n;
 
-                if ( (n=accept(socket_listening, NULL, NULL)) < 0 )
+                if ((n=accept(socket_listening, NULL, NULL)) < 0)
                     perror("accept") ;
-                else if ( insert_socket_into_list(n) == 1 ) {
+                else if (insert_socket_into_list(n) == 1) {
                     send(n,SERVER_BUSY,strlen(SERVER_BUSY),0) ;
                     close(n) ;
                 }
-                continue ;
+                continue;
             } 
             else {
-                int i ;
+                int i;
 
-                for ( i = 0; i < max_users; i++ )
-                    if ( FD_ISSET(list_of_sockets[i], &select_set))
-                        if ( get_message_from_socket(list_of_sockets[i]) == 0 )
-                            send_message_to_all(list_of_sockets[i]) ;
+                for (i = 0; i < max_users; i++)
+                    if (FD_ISSET(list_of_sockets[i], &select_set))
+                        if (get_message_from_socket(list_of_sockets[i]) == 0)
+                            send_message_to_all(list_of_sockets[i]);
             }
         }
     } 
-    return 0 ;
+    return (0);
 } 
